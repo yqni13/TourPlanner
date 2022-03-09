a4dced6bcc03b86e4e37d8f1192218af77ad0f06
yqni13 <yqni13@protonmail.com>
2022-03-09 11:22:21 +0100

    preparation mediator pattern

---
+++ TourPlanner/TourPlanner/UI/ViewModels/TourOverviewMediator/TourOverviewViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TourPlanner.UI.ViewModels.AbstractMediator;
+
+namespace TourPlanner.UI.ViewModels
+{
+    internal class TourOverviewViewModel : BaseViewModel
+    {
+        private string _result;
+
+        public string Result
+        {
+            get => _result;
+            set
+            {
+                _result = value;
+                OnPropertyChanged();
+            }            
+        }
+
+        public void DisplayTourDataOverview(string result)
+        {
+            Result = result;
+        }
+    }
+}
