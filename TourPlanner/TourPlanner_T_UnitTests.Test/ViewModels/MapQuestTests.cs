16f6e7421c3fd7124fca997c0a5f84e4f588dec3
yqni13 <65848806+yqni13@users.noreply.github.com>
2022-04-26 08:15:31 +0200

    update

---
+++ TourPlanner/TourPlanner_T_UnitTests.Test/ViewModels/MapQuestTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TourPlanner_T_UnitTests.Test.ViewModels
+{
+    public class MapQuestTests
+    {
+
+
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        
+
+    }
+}
