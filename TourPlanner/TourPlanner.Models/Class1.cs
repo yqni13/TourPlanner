499b60b1eb661c8fe81ec63cb2536566e66adda3
Tobi <if20b125@technikum-wien.at>
2022-04-27 11:27:01 +0200

    Auto stash before merge of "development" and "origin/development"

--- TourPlanner/TourPlanner.Models/Class1.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace TourPlanner.Models
-{
-    public class Class1
-    {
-    }
-}
---
+++ TourPlanner/TourPlanner.Models/Tour.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TourPlanner.Models
+{
+    class Tour
+    {
+        string name;
+        string description;
+        Adress from;
+        Adress to;
+        TransportType transport;
+        double distance;
+        TimeSpan duration; //needs testing see if we can calculate average of list of TimeSpan (adding and subtracting two TimeSpan should work)
+        
+        /*
+         * Missing: the image 
+         */
+        private List<TourLogs> _logs = new List<TourLogs>();
+
+
+    }
+}
