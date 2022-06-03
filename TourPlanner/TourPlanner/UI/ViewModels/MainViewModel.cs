e0e23b63837fa1fd6234d29036ec9f6e5c00482a
Tobi <to.meindl@gmx.at>
2022-06-03 11:47:09 +0200

    delete tours and fixed update of result list

--- TourPlanner/TourPlanner/UI/ViewModels/MainViewModel.cs
+++ TourPlanner/TourPlanner/UI/ViewModels/MainViewModel.cs
@@ -4,6 +4,7 @@ using System.Windows;
 using TourPlanner.DAL;
 using TourPlanner.Models;
 using TourPlanner.UI.TourSearch;
+using TourPlanner.UI.ViewComponents;
 using TourPlanner.UI.ViewModels.AbstractMediator;
 
 
@@ -14,8 +15,18 @@ namespace TourPlanner.UI.ViewModels.TourOverviewMediator
         private readonly TourOverviewViewModel resultView;
         private readonly ISearch searchEngine;        
 
-        public Collection<Tour> Data { get; }
+        public Collection<Tour> Data { get; set; }
            = new Collection<Tour>();
+
+        public Window OpenInputWindow { get; set; }
+
+        public SearchBarViewModel SearchBar { get; set; }
+        public TourDataResultsViewModel ResultView { get; set; }
+        public TourOverviewViewModel DetailView { get; set; }
+        public AddTourViewModel AddTour { get; set; }
+        public MenuViewModel Menu { get; set; }
+
+
 
         public MainViewModel(SearchBarViewModel searchBar,
                             TourDataResultsViewModel resultView,
@@ -24,26 +35,68 @@ namespace TourPlanner.UI.ViewModels.TourOverviewMediator
                             MenuViewModel menu
                             )
         {
+            this.SearchBar = searchBar;
+            this.ResultView = resultView;
+            this.DetailView = detailView;
+            this.AddTour = addTour;
+            this.Menu = menu;
 
-            searchBar.SearchTextChanged += (_, searchText) =>
+            //subscribe to all the Events from the ViewModels 
+            SubscribeToEvents();
+
+
+            //populate the list of tours in the result view 
+            this.Data = TourAccess.getTours();
+            resultView.UpdateTours(this.Data);            
+        }
+
+        private void SubscribeToEvents()
+        {
+            ResultView.SelectedTourChanged += (_, tour) =>
+            {
+                DetailView.SelectedTour = tour;
+                if (tour == null)
+                {
+                    MessageBox.Show("no tour selected");
+                }
+                else
+                {
+                    MessageBox.Show("New SelectedTour");
+                }
+                
+            };
+            ResultView.OpenAddDialogEvent += (_, arg) =>
+            {
+                OpenAddDialog();
+            };
+            ResultView.DeleteTourEvent += (_, tour) =>
+            {
+                MessageBox.Show("Delete Tour with UUID " + tour.ID.ToString());
+                TourAccess.DeleteTour(tour.ID);
+                this.Data = TourAccess.getTours();
+                ResultView.UpdateTours(this.Data);
+            };
+            SearchBar.SearchTextChanged += (_, searchText) =>
             {
                 MessageBox.Show("SeachTextChanged called");
-                SearchTours(searchText);                
+                SearchTours(searchText);
             };
-            //this.resultView = resultView;
-            //this.searchEngine = searchEngine;
+            AddTour.AddedTourEvent += (_, tour) =>
+            {                
+                
+                TourAccess.AddTour(tour);
+                this.Data = TourAccess.getTours();                
+                ResultView.UpdateTours(this.Data);
+                CloseOpenWindow();
+                              
+            };
+            AddTour.CloseAddTourDialogEvent += (_, arg) =>
+            {
+                CloseOpenWindow();
+            };
 
-            addTour.ToursChangedEvent += (_, newTourName) =>
-            {
-                MessageBox.Show("ToursChangedEvent called");
-                resultView.UpdateTours(TourAccess.getTours());
-            };
-            resultView.SelectedTourChanged += (_, tour) =>
-            {
-                detailView.SelectedTour = tour;
-                MessageBox.Show("New SelectedTour");
-            };
-            resultView.UpdateTours(TourAccess.getTours());
+
+
             
         }
 
@@ -51,6 +104,18 @@ namespace TourPlanner.UI.ViewModels.TourOverviewMediator
         {
             //var results = String.Join("\n", this.searchEngine.TourSearch(searchText));
             //this.resultView.DisplayTourDataOverview(results);
+        }
+
+        private void OpenAddDialog()
+        {
+            OpenInputWindow = new AddTourDialog();
+            OpenInputWindow.DataContext = this.AddTour;
+            OpenInputWindow.Show();            
+        }
+
+        private void CloseOpenWindow()
+        {
+            OpenInputWindow.Hide();   
         }
     }
 }
--- TourPlanner/TourPlanner/UI/ViewModels/SubViewModels/AddTourViewModel.cs
+++ TourPlanner/TourPlanner/UI/ViewModels/SubViewModels/AddTourViewModel.cs
@@ -12,26 +12,46 @@ namespace TourPlanner.UI.ViewModels.TourOverviewMediator
 {
     public class AddTourViewModel
     {
+        public Tour newTour { get; set; } = new Tour();     
+        /*
         public string TourName { get; set; } = "Test";
         public string TourDescription { get; set; } = "Des...";
         public Adress From { get; set; } = new Adress("bla");
         public Adress To { get; set; } = new Adress("bla");
 
         public String TransportType { get; set; } = "Bike";
+        */
 
         //Subscribe to this in order to get notivied of event 
-        public event EventHandler<string> ToursChangedEvent;
+        public event EventHandler<Tour> AddedTourEvent;
+        public event EventHandler CloseAddTourDialogEvent;
 
-        public ICommand AddTourCommand { get; }        
+        public ICommand AddTourCommand { get; }
+        public ICommand CloseDialogCommand { get; }
 
         public AddTourViewModel()
         {
+            newTour.Name = "testTour";
+            newTour.Description = "Desc....";
+            newTour.From.Street = "Straße";
+            newTour.From.Number = "31/5";
+            newTour.From.City = "Wien";
+            newTour.From.ZibCode = 1200;
+            newTour.From.Country = "AT";
+            newTour.To.Street = "Straße";
+            newTour.To.Number = "31/5";
+            newTour.To.City = "Wien";
+            newTour.To.ZibCode = 1200;
+            newTour.To.Country = "AT";
             //AddTourCommand function raises the ToursChangedEvent event when called            
             AddTourCommand = new RelayCommand((_) =>
+            {                
+                this.AddedTourEvent?.Invoke(this, newTour);
+                this.newTour = new Tour();
+            });
+            CloseDialogCommand = new RelayCommand((_) =>
             {
-                TourAccess.AddTour(new Tour(TourName, TourDescription, From, To));
-                MessageBox.Show("test");
-                this.ToursChangedEvent?.Invoke(this, TourName);
+                this.CloseAddTourDialogEvent?.Invoke(this, EventArgs.Empty);
             });
         }
 
