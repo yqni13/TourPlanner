ad3ef46816bc1cfcd17e1d377624c88f3273d9c6
Tobi <to.meindl@gmx.at>
2022-06-05 11:02:42 +0200

    Adress parsing

--- TourPlanner/TourPlanner.Models/Adress.cs
+++ TourPlanner/TourPlanner.Models/Adress.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace TourPlanner.Models
 {
@@ -26,24 +27,23 @@ namespace TourPlanner.Models
         {
         }
         public Adress(String s)
-        {
-            this.Street = "test";
-            this.Number = "numb";
-            this.ZibCode = 1230;
-            this.City = "Wien";
-            this.Country = "AT";
-            /*
-            string[] substrings = s.Split(' ', ',', StringSplitOptions.RemoveEmptyEntries);
-            this.Street = substrings[0];
-            this.Number = substrings[1];
-            this.ZibCode = Int32.Parse(substrings[2]);
-            this.City = substrings[3];
-            */
+        {   
+            //define the characters by witch to seperate
+            char[] seperators = new char[] { ' ', ',' };
+
+            string[] substrings = s.Split(seperators, StringSplitOptions.RemoveEmptyEntries);
+            
+            //Check if the splitted array contains item at that point in case the string was not as long as expected
+            this.Street = substrings.ElementAtOrDefault(0) != null ? substrings[0] : "";
+            this.Number = substrings.ElementAtOrDefault(1) != null ? substrings[1] : "";
+            this.ZibCode = substrings.ElementAtOrDefault(2) != null ? Int32.Parse(substrings[2]) : 0000; 
+            this.City = substrings.ElementAtOrDefault(3) != null ? substrings[3] : "";
+            this.Country = substrings.ElementAtOrDefault(4) != null ? substrings[4] : "";
         }
 
         public override String ToString()
         {
-            return this.Street + " " + this.Number + ", " + this.ZibCode + " " + this.City; 
+            return this.Street + " " + this.Number + ", " + this.ZibCode + " " + this.City + ", " + this.Country; 
         }
     }
 }
